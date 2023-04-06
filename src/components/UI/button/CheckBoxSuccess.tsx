26bdb1eb593fd4818ab7c946cb7e97df454ef01d
Dilmurat <dilmuratsarybaev@gmail.com>
2023-04-06 19:43:35 +0600

    radio_checkbox

---
+++ src/components/UI/button/CheckBoxSuccess.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled'
+import { Checkbox } from '@mui/material'
+const StyledCheckBox = styled(Checkbox)({
+  color: '#858FA4',
+  '&.Mui-checked': {
+    color: '#2FC509'
+  }
+})
+
+export const CheckBoxSucces = () => {
+  return <StyledCheckBox />
+}
---
+++ src/components/UI/button/Radio.tsx
@@ -0,0 +1,15 @@
+import { styled } from '@mui/material'
+import Radio from '@mui/material/Radio'
+
+const StyledRadio = styled(Radio)({
+  '&:hover': {
+    color: '#CB11AB'
+  },
+  '&.Mui-checked': {
+    color: '#CB11AB'
+  },
+  color: '#858FA4'
+})
+export const RadioButton = () => {
+  return <StyledRadio />
+}
