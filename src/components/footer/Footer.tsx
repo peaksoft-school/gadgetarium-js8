f51ae0b9115c128a6a372bf2aa353600608ab2e8
Nurbolot <nurbolotu9@gmail.com>
2023-04-09 18:03:39 +0600

    footer was completed

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -13,106 +13,156 @@ const StyledFooter = styled('footer')(() => ({
   color: '#858FA4',
   paddingBottom: '2.5rem',
   paddingTop: '3.75rem',
-  paddingLeft: '12.1rem',
-  paddingRight: '12.1rem'
+  paddingLeft: '7rem',
+  paddingRight: '7rem'
 }))
 
 const StyledLink = styled('a')(() => ({
   textDecoration: 'none',
   color: '#858FA4',
+  fontFamily: 'Inter',
+  fontStyle: 'normal',
+  fontWeight: 500,
+  fontSize: '16px',
+  lineHeight: '130%',
   '&:hover': {
     color: '#FFF'
   }
 }))
 
-const StyledFirstFooterBlock = styled('a')(() => ({
-  display: 'flex'
+const StyledBlockName = styled('p')(() => ({
+  color: '#fff',
+  marginBottom: '2rem'
+}))
+
+const StyledFirstSubFooterBlock = styled('div')(() => ({
+  display: 'flex',
+  justifyContent: 'space-between',
+  paddingRight: '10rem',
+  width: '60%',
+  li: {
+    listStyle: 'none',
+    marginBottom: '0.75rem'
+  }
+}))
+
+const StyledFirstFooterBlock = styled('div')(() => ({
+  borderBottom: '1.5px solid #858FA426',
+  display: 'flex',
+  justifyContent: 'space-around',
+  paddingBottom: '3.5rem',
+  marginBottom: '2.2rem'
+}))
+
+const InfoBlock = styled('div')(() => ({
+  marginTop: '2.625rem',
+  p: {
+    display: 'flex',
+    alignItems: 'center',
+    margin: '1rem 10px'
+  }
+}))
+
+const StyledSecondFooterBlock = styled('div')(() => ({
+  width: '30%'
+}))
+
+const StyledThirdFooterBlock = styled('div')(() => ({
+  textAlign: 'center'
 }))
 
 const Footer = () => {
   return (
     <StyledFooter>
       <StyledFirstFooterBlock>
-        <div>
-          <p>Каталог</p>
-          <ul>
-            <li>
-              <StyledLink href="#">Смартфоны</StyledLink>
-            </li>
-            <li>
-              <StyledLink href="#">Ноутбуки и планшеты</StyledLink>
-            </li>
-            <li>
-              <StyledLink href="#">Смарт-часы и браслеты</StyledLink>
-            </li>
-            <li>
-              <StyledLink href="#">Аксессуары</StyledLink>
-            </li>
-          </ul>
-        </div>
+        <StyledFirstSubFooterBlock>
+          <div>
+            <StyledBlockName>Каталог</StyledBlockName>
+            <ul>
+              <li>
+                <StyledLink href="#">Смартфоны</StyledLink>
+              </li>
+              <li>
+                <StyledLink href="#">Ноутбуки и планшеты</StyledLink>
+              </li>
+              <li>
+                <StyledLink href="#">Смарт-часы и браслеты</StyledLink>
+              </li>
+              <li>
+                <StyledLink href="#">Аксессуары</StyledLink>
+              </li>
+            </ul>
+          </div>
 
-        <div>
-          <p>Будь с нами</p>
-          <ul>
-            <li>
-              <StyledLink href="#">Акции</StyledLink>
-            </li>
-            <li>
-              <StyledLink href="#">Новинки</StyledLink>
-            </li>
-            <li>
-              <StyledLink href="#">Популярные категории</StyledLink>
-            </li>
-          </ul>
-        </div>
+          <div>
+            <StyledBlockName>Будь с нами</StyledBlockName>
+            <ul>
+              <li>
+                <StyledLink href="#">Акции</StyledLink>
+              </li>
+              <li>
+                <StyledLink href="#">Новинки</StyledLink>
+              </li>
+              <li>
+                <StyledLink href="#">Популярные категории</StyledLink>
+              </li>
+            </ul>
+          </div>
 
-        <div>
-          <p>Помощь и сервисы</p>
-          <ul>
-            <li>
-              <StyledLink href="#">О магазине</StyledLink>
-            </li>
-            <li>
-              <StyledLink href="#">Доставка</StyledLink>
-            </li>
-            <li>
-              <StyledLink href="#">FAQ</StyledLink>
-            </li>
-            <li>
-              <StyledLink href="#">Контакты</StyledLink>
-            </li>
-          </ul>
-        </div>
+          <div>
+            <StyledBlockName>Помощь и сервисы</StyledBlockName>
+            <ul>
+              <li>
+                <StyledLink href="#">О магазине</StyledLink>
+              </li>
+              <li>
+                <StyledLink href="#">Доставка</StyledLink>
+              </li>
+              <li>
+                <StyledLink href="#">FAQ</StyledLink>
+              </li>
+              <li>
+                <StyledLink href="#">Контакты</StyledLink>
+              </li>
+            </ul>
+          </div>
+        </StyledFirstSubFooterBlock>
 
-        <div>
-          <p>Расскажем об акциях и скидках</p>
+        <StyledSecondFooterBlock>
+          <StyledBlockName>Расскажем об акциях и скидках</StyledBlockName>
           <input type="email" />
           <button>Подпиcаться</button>
-          <h5>Нажимая на кнопку «подписаться» Вы соглашаетесь на обработку персональных данных</h5>
-          <p>
-            <Telephone /> +996 (400) 00 00 00
+          <p style={{ color: '#FFF' }}>
+            Нажимая на кнопку «подписаться» Вы соглашаетесь на обработку персональных данных
           </p>
-          <p>
-            <Mail />
-            Gadgetarium.kg
-          </p>
-          <p>
-            <Location /> г.Бишкек, ул. Гражданская 119
-          </p>
-          <p>
-            <Clock />С 10:00 до 21:00 (без выходных)
-          </p>
-        </div>
+          <InfoBlock>
+            <p>
+              <Telephone /> +996 (400) 00 00 00
+            </p>
+            <p>
+              <Mail />
+              Gadgetarium.kg
+            </p>
+            <p>
+              <Location /> г.Бишкек, ул. Гражданская 119
+            </p>
+            <p>
+              <Clock />С 10:00 до 21:00 (без выходных)
+            </p>
+          </InfoBlock>
+        </StyledSecondFooterBlock>
       </StyledFirstFooterBlock>
 
-      <div>
+      <StyledThirdFooterBlock>
         <div>
           <a href="/">
             <Logo />
           </a>
         </div>
-        <p>© 2022 Gadgetarium. Интернет магазин Все права защищены.</p>
-      </div>
+        <p>
+          © 2022 Gadgetarium. Интернет магазин <br /> Все права защищены.
+        </p>
+      </StyledThirdFooterBlock>
     </StyledFooter>
   )
 }
