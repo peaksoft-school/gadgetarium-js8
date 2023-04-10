7cb92868a5efa4c1c0fb15093c207e2eee776e0f
Dilmurat <dilmuratsarybaev@gmail.com>
2023-04-10 11:34:45 +0600

    completed-radio-checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
 import './App.css'
-import { CheckBoxLabels } from './components/UI/button/CheckBox'
-import { CheckBoxSucces } from './components/UI/button/CheckBoxSuccess'
-import { RadioButton } from './components/UI/button/Radio'
 
 function App() {
-  return (
-    <div>
-      <CheckBoxLabels />
-      <RadioButton />
-      <CheckBoxSucces />
-    </div>
-  )
+  return <div></div>
 }
 
 export default App
