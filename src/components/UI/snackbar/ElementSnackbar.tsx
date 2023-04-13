ecbca5622af672a357f42a8d4c9f667fd6fd2086
Dilmurat <dilmuratsarybaev@gmail.com>
2023-04-13 18:23:42 +0600

    completed snackbar

---
+++ src/components/UI/snackbar/ElementSnackbar.tsx
@@ -0,0 +1,38 @@
+import { styled } from '@mui/material'
+import { ReactComponent as SnackbarIcon } from '../../../assets/icons/SnackbarIcon.svg'
+import { SnackbarType } from './Snackbar'
+const Container = styled('div')(() => ({
+  width: 'auto',
+  fontFamily: 'Inter',
+  fontStyle: 'normal',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  gap: 22
+}))
+const Title = styled('p')(() => ({
+  width: 'auto',
+  fontWeight: 400,
+  fontSize: '18px',
+  lineHeight: ' 140%',
+  color: '#FFFFFF'
+}))
+const StyledLink = styled('a')(() => ({
+  textDecoration: 'none',
+  fontWeight: 700,
+  fontSize: '18px',
+  lineHeight: '130%',
+  color: '#3CDE14;'
+}))
+const StyledSnackbarIcon = styled(SnackbarIcon)(() => ({
+  marginRight: 12
+}))
+export const ElementSnackbar = ({ message, linkText }: SnackbarType) => {
+  return (
+    <Container>
+      <Title>{message}</Title>
+      <StyledLink href="#">{linkText}</StyledLink>
+      <StyledSnackbarIcon />
+    </Container>
+  )
+}
--- src/components/UI/snackbar/Snackbar.tsx
+++ src/components/UI/snackbar/Snackbar.tsx
@@ -1,80 +1,53 @@
-import Snackbar from '@mui/material/Snackbar'
-import { ReactComponent as SnackbarIcon } from '../../../assets/icons/SnackbarIcon.svg'
-import styled from '@emotion/styled'
-import { Fragment } from 'react'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { ElementSnackbar } from './ElementSnackbar'
+import { Fragment, useEffect } from 'react'
+import { styled } from '@mui/material'
 
-type SnackbarOriginType = {
-  vertical: 'top' | 'bottom'
-  horizontal: 'left' | 'center' | 'right'
-}
-
-interface SnackbarType {
-  isOpen?: boolean
-  autoHideDuration?: number
-  onClose?: () => void
+export interface SnackbarType {
   message?: string
-  text?: string
-  anchorOrigin?: SnackbarOriginType
+  linkText?: string
+  position?:
+    | 'top-left'
+    | 'top-center'
+    | 'top-right'
+    | 'bottom-left'
+    | 'bottom-center'
+    | 'bottom-right'
+  type?: 'success' | 'error'
+  autoClose?: number
 }
 
-const SimpleSnackbar = ({
-  isOpen = true,
-  autoHideDuration,
-  onClose,
+export const Snackbar = ({
   message = 'Товар успешно добавлен в корзину!',
-  text = 'Перейти в корзину',
-  anchorOrigin = { vertical: 'top', horizontal: 'right' }
+  linkText = 'Перейти в корзину',
+  position = 'top-right',
+  type = 'success',
+  autoClose = 4000
 }: SnackbarType) => {
-  const Container = styled('div')(() => ({
-    marginLeft: '0.8rem',
-    display: 'flex',
-    alignItems: 'center'
-  }))
-  const StyledLink = styled('a')(() => ({
-    fontFamily: 'Inter',
-    fontStyle: 'normal',
-    fontWeight: 700,
-    fontSize: '1.125rem',
-    lineHeight: '130%',
-    color: '#3CDE14',
-    textDecoration: 'none'
-  }))
-  const StyledSnackbarIcon = styled(SnackbarIcon)(() => ({
-    marginLeft: '30.95px'
-  }))
+  const snackbarHanler = () => {
+    toast[type](<ElementSnackbar message={message} linkText={linkText} />, {
+      style: {
+        backgroundColor: '#202027'
+      }
+    })
+  }
 
-  const action = (
-    <Container>
-      <StyledLink href="#">{text}</StyledLink>
-      <StyledSnackbarIcon onClick={onClose} />
-    </Container>
-  )
+  useEffect(() => {
+    snackbarHanler()
+  }, [type])
 
+  const Toastify = styled(ToastContainer)(() => ({
+    '&': {
+      width: 'auto',
+      '& .Toastify__close-button': {
+        display: 'none'
+      }
+    }
+  }))
   return (
     <Fragment>
-      <Snackbar
-        ContentProps={{
-          sx: {
-            fontFamily: 'Inter',
-            fontStyle: 'normal',
-            fontWeight: 400,
-            fontSize: '1.125rem',
-            lineHeight: '140%',
-            textAlign: 'center',
-            color: '#FFFFFF',
-            backgroundColor: '#202027',
-            borderRadius: '4px',
-            padding: '10px 28.85px 10px 18px'
-          }
-        }}
-        open={isOpen}
-        anchorOrigin={anchorOrigin}
-        autoHideDuration={autoHideDuration}
-        onClose={onClose}
-        message={message}
-        action={action}
-      />
+      <Toastify position={position} autoClose={autoClose} icon={false} />
     </Fragment>
   )
 }
-export default SimpleSnackbar
