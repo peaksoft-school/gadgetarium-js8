c30f899f9b595958df7a17c433ee31bd704f92bd
Dilmurat <dilmuratsarybaev@gmail.com>
2023-05-16 16:48:08 +0600

    forget to clear App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,11 @@ import AppRoutes from './routes/AppRoutes'
 import { ThemeProvider } from '@mui/material'
 import { appTheme } from './utils/constants/theme/theme'
 import { store } from './redux/store'
-import { useState } from 'react'
-import Banner from './components/admin/UI/banners/Banner'
+
 const AppContent = () => {
-  const [state, setState] = useState(false)
-  const render = () => {
-    setState((prevState) => !prevState)
-  }
   return (
     <div>
-      <Banner isOpen={state} onClose={render} />
-      <button onClick={render}>click me</button>
-      {/* <AppRoutes /> */}
+      <AppRoutes />
     </div>
   )
 }
