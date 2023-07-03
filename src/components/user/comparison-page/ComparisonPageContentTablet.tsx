da2d04a9dd9ef1ccddbc608bd74c579ccfeb5c11
Feruza <122290607+FeruzaKazakova@users.noreply.github.com>
2023-07-03 17:44:39 +0600

    i can't describe this actions

--- src/components/user/comparison-page/ComparisonPageContentTablet.tsx
+++ src/components/user/comparison-page/ComparisonPageContentTablet.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useEffect, useRef, useState } from 'react'
 import * as ITEMS from './compareItems'
 import { CompareProducts } from '../../../api/compare-products/compareProductsService'
